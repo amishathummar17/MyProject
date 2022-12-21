fc178eede056174f3cb53563e8ed9f25e3d13f12
amisha <athumar847@rku.ac.in>
2022-12-21 10:59:36 +0530

    delete some field

--- sale_extend/models/sale_order.py
+++ sale_extend/models/sale_order.py
@@ -4,5 +4,5 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     customer_comment = fields.Char(string="Customer Comment")
-    maintain_partner_id = fields.Many2one('res.partner', string="Maintain Partner")
+
 
